344bdbee2a63837c21cee4a2420be831fa7fba31
Pawan Kumar <34783432+Pawan0987@users.noreply.github.com>
2025-01-11 04:08:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 class Config:
     YOLO_WEIGHTS = "yolov5s.pt"
     YOLO_CONFIG = "yolov5s.yaml"
-    VIDEO_PATH = "mumbai_traffic.mp4"
-    OUTPUT_PATH = "output"
+    VIDEO_PATH = "samplevideo.mp4"
+    OUTPUT_PATH = "object_detection_output.json"
 
     @classmethod
     def create_output_directory(cls):
